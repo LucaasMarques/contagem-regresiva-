db024ad94343a8602ef9a3c53ed3dc2e7e3477a8
Lucas Marques <lucsalmeida004@gmail.com>
2025-09-05 22:19:24 -0300

    Finaliza exercício.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("FOGO!");
+Console.ResetColor();
